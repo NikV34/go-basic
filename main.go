c6089cd0ab634745aa4dffac01df152d17d73f7b
NikV34 <25172610+NikV34@users.noreply.github.com>
2021-04-01 22:52:17 +0200

    1.13 шаг 4

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main()  {
-	var N int
-	fmt.Scan(&N)
-	fmt.Printf("It is %d hours %d minutes.", N/3600, N%3600/60)
+	var a,b,c int
+	fmt.Scan(&a,&b,&c)
+	if a*a + b*b == c*c {
+		fmt.Print("Прямоугольный")
+	} else {
+		fmt.Print("Непрямоугольный")
+	}
 }
