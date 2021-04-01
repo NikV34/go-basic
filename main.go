af3f8e8882afd932fd6fc511ef6375e4df63e797
NikV34 <25172610+NikV34@users.noreply.github.com>
2021-04-01 22:30:22 +0200

    counter

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main()  {
-	array := []int{}
 	var N, a int
+	var counter = 0
 	fmt.Scan(&N)
+	array := []int{}
 	for i:=0; i < N; i++{
 		fmt.Scan(&a)
-		if i%2==0 {
-			array = append(array, a)
+		array = append(array,a)
+		if array[i] > 0 {
+			counter++
 		}
 	}
-	for _,value := range array {
-		fmt.Print(value)
-	}
+	fmt.Print(counter)
 }
