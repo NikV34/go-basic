79afce8784956d9db6f4d1e7cec732594ed44f39
NikV34 <25172610+NikV34@users.noreply.github.com>
2021-04-01 22:47:26 +0200

    1.13-4

--- main.go
+++ main.go
@@ -5,12 +5,5 @@ import "fmt"
 func main()  {
 	var N int
 	fmt.Scan(&N)
-	array := []int{}
-	for ; N > 0; {
-		array = append(array,N%10)
-		N=N/10
-	}
-	for _,value := range array {
-		fmt.Print(value)
-	}
+	fmt.Printf("It is %d hours %d minutes.", N/3600, N%3600/60)
 }
