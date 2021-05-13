1b678dd18f33aa9a5ed77ac539eaf4b48197c169
NikV34 <25172610+NikV34@users.noreply.github.com>
2021-05-14 00:02:25 +0200

    adds simple web framework and websocket

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
 
-func main()  {
-	var a,b,c int
-	fmt.Scan(&a,&b,&c)
-	if a*a + b*b == c*c {
-		fmt.Print("Прямоугольный")
-	} else {
-		fmt.Print("Непрямоугольный")
-	}
+func main() {
+	r := gin.Default()
+	m := melody.New()
+
+	r.Use(static.Serve("/", static.LocalFile("./public", true)))
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	r.Run(":5000")
 }
